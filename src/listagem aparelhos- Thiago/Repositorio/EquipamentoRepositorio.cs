6401f23fe3c90215a9f300c69a56fb9d5a142b92
Thiago <115198478+ThiagoOliveiraQ@users.noreply.github.com>
2023-05-07 20:49:27 -0300

    Add files via upload

---
+++ src/listagem aparelhos- Thiago/Repositorio/EquipamentoRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using WebApplication2.Context;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Repositorio
+{
+    public class EquipamentoRepositorio : IEquipamentoRepositorio
+    {
+        private readonly AppDbContext _appdbcontext;
+        public EquipamentoRepositorio(AppDbContext appdbcontext)
+        {
+            this._appdbcontext = appdbcontext;
+        }
+        public EquipamentoModel ListarPorId(int id)
+        {
+            return _appdbcontext.Equipamentos.FirstOrDefault(x => x.Id == id);
+        }
+        public List<EquipamentoModel> BuscarTodos()
+        {
+            return _appdbcontext.Equipamentos.ToList();
+        }
+        public EquipamentoModel Adicionar(EquipamentoModel equipamento)
+        {
+            _appdbcontext.Equipamentos.Add(equipamento);
+            _appdbcontext.SaveChanges();
+
+            return equipamento;
+        }
+
+        
+    }
+}
---
+++ src/listagem aparelhos- Thiago/Repositorio/IEquipamentoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repositorio
+{
+    public interface IEquipamentoRepositorio
+    {
+        EquipamentoModel ListarPorId(int id);
+        List<EquipamentoModel> BuscarTodos();
+        EquipamentoModel Adicionar(EquipamentoModel equipamento);
+    }
+}
