1eb48a3cae934d97afab29671a9634ca240a63e3
Thiago <115198478+ThiagoOliveiraQ@users.noreply.github.com>
2023-05-21 16:49:57 -0300

    Add files via upload

---
+++ src/Listagem Thiago/Data/BancoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Context;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EquipamentoModel> Equipamentos { get; set; }
+
+    }
+}
+
