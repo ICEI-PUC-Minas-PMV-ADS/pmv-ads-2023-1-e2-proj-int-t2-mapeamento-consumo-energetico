f1a6d1830f184928d7a8d7084cb76a292fc94b5f
Thiago <115198478+ThiagoOliveiraQ@users.noreply.github.com>
2023-05-21 16:51:39 -0300

    Add files via upload

---
+++ src/Listagem Thiago/Repositorio/IEquipamentoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repositorio
+{
+    public interface IEquipamentoRepositorio
+    {
+        EquipamentoModel ListarPorId(int id);
+        List<EquipamentoModel> BuscarTodos();
+        EquipamentoModel Adicionar(EquipamentoModel equipamento);
+        EquipamentoModel Atualizar(EquipamentoModel equipamento);
+        bool ApagarReal(int id);
+    }
+}
